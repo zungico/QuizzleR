9b7f4096cb61252c7885c770af7d3501376ace05
Vladimir Alekseev <vovauu@icloud.com>
2023-01-31 18:41:30 +0300

    Initial Commit

---
+++ QuizzleR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizzleR
+//
+//  Created by Вова on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
